41843660663d82f971f5cc945a5945fbfaf476e7
MaarksN <130610135+MaarksN@users.noreply.github.com>
2026-04-12 01:02:58 -0300

    Update index.ts

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,6 +2,15 @@
 // 
 export * from "@prisma/client";
 export * from "./client.js";
+export enum SessionAccessMode {
+  REGULAR = "REGULAR",
+  BREAK_GLASS = "BREAK_GLASS"
+}
+export enum RetentionExecutionMode {
+  AUTOMATED = "AUTOMATED",
+  MANUAL = "MANUAL",
+  LEGAL_HOLD = "LEGAL_HOLD"
+}
 export * from "./errors/exceeded-quota.error.js";
 export * from "./errors/prisma-query-timeout.error.js";
 export * from "./errors/tenant-required.error.js";
